8d6b866209b90c7cb94b3e22db434708ce5976c4
lzb991435344 <991435344@qq.com>
2020-03-09 08:46:44 +0800

    function strtok
    函数 strtok

--- CAndPointer/ch9/token.c
+++ CAndPointer/ch9/token.c
@@ -10,7 +10,9 @@ print_tokens( char *line )
 {
 	static	char	whitespace[] = " \t\f\r\v\n";
 	char	*token;
-
+	//分解字符串 str 为一组字符串，delim 为分隔符
+	//char *strtok(char *str, const char *delim)
+	//该函数返回被分解的第一个子字符串，如果没有可检索的字符串，则返回一个空指针
 	for( token = strtok( line, whitespace );
 	    token != NULL;
 	    token = strtok( NULL, whitespace ) )
